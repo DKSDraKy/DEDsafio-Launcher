f3020373a3b3b525032ad71fcb34c3f2a528c675
DKSDraKy <81652778+DKSDraKy@users.noreply.github.com>
2024-10-27 02:16:28 -0600

    Update instances.php

--- files/php/instances.php
+++ files/php/instances.php
@@ -1,7 +1,7 @@
 <?php
-$instance['hypixel'] = array_merge($instance['hypixel'], array(
+$instance['dedsafio'] = array_merge($instance['dedsafio'], array(
     "loadder" => array(
-        "minecraft_version" => "1.8.9",
+        "minecraft_version" => "1.16.5",
         "loadder_type" => "forge",
         "loadder_version" => "latest"
     ),
@@ -21,26 +21,8 @@ $instance['hypixel'] = array_merge($instance['hypixel'], array(
     "whitelist" => array(),
     "whitelistActive" => false,
     "status" => array(
-        "nameServer" => "Hypixel",
-        "ip" => "mc.hypixel.net",
-        "port" => 25565
+        "nameServer" => "DEDsafio",
+        "ip" => "23.230.3.50",
+        "port" => 25570
     )
 ));
-
-$instance['PokeMoonX'] = array_merge($instance['PokeMoonX'], array(
-    "loadder" => array(
-        "minecraft_version" => "1.16.5",
-        "loadder_type" => "forge",
-        "loadder_version" => "1.16.5-36.2.35"
-    ),
-    "verify" => false,
-    "ignored" => array(),
-    "whitelist" => array(),
-    "whitelistActive" => false,
-    "status" => array(
-        "nameServer" => "PokeMoonX",
-        "ip" => "178.32.106.234",
-        "port" => 25599
-    )
-));
-?>
